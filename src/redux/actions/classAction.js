453edb45f7229b252d94f6c6bda493f508d6e46f
sendeveloper <lightmorning46@gmail.com>
2019-02-13 06:48:55 -0800

    Fixed rest api url issue

--- src/redux/actions/classAction.js
+++ src/redux/actions/classAction.js
@@ -227,7 +227,7 @@ export const dashBordData = () => dispatch => {
 };
 export const getClassById = classId => dispatch => {
   dispatch(showLoadmask());
-  const getClass = `${baseURL + REST_API.Class.GetClasses}?id=${classId}`;
+  const getClass = `${baseURL + REST_API.Class.GetClass}?id=${classId}`;
   axios
     .get(getClass)
     .then(response => {
